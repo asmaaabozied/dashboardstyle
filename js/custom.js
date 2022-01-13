c6a0f069ff01c81f27458c84f84f64a3247e53b0
asmaaabozied <asmaaabozied907@gmail.com>
2022-01-13 16:34:47 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+	$("a.navbar-brand").css({
+	  'width': ($("#sidebar-collapse").width() + 'px')
+	});
+  });
+  $(window).resize(function(){
+	$("a.navbar-brand").css({
+		'width': ($("#sidebar-collapse").width() + 'px')
+	  });
+  });
+
+$('#calendar').datepicker({
+		});
+
+!function ($) {
+    $(document).on("click","ul.nav li.parent > a ", function(){          
+        $(this).find('em').toggleClass("fa-minus");      
+    }); 
+    $(".sidebar span.icon").find('em:first').addClass("fa-plus");
+}
+
+(window.jQuery);
+	$(window).on('resize', function () {
+  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+})
+$(window).on('resize', function () {
+  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+})
+
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('em').removeClass('fa-toggle-up').addClass('fa-toggle-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('em').removeClass('fa-toggle-down').addClass('fa-toggle-up');
+	}
+})
